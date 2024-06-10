5b266f92e037e67acbc3df001465f147cdf171a4
namoh11 <namoh@ualberta.ca>
2024-06-10 14:48:38 -0600

    i don't think my last commit worked but i can run route from page to page and run the playback function

--- main_flask.py
+++ main_flask.py
@@ -1,12 +1,25 @@
-from flask import Flask ,render_template
+from main import userPlayback
+from flask import Flask ,render_template, request, redirect, url_for
+
 app = Flask(__name__)
+
 @app.route('/', methods = ['GET','POST'])
 def menu():
     return render_template('menu.html')
 
-@app.route('/trackhistory', methods = ['POST'])
-def trackHistory():
-    numOfTracks = request.args.get('numOfTracks')
-    numOfTracks = int(numOfTracks)
-    # how do i print out the dictionary of songs without printing a dictionary lmaooo
+@app.route('/listeninghistory', methods=['GET'])
+def listening_history():
     return render_template('listeningHistory.html')
+
+# @app.route('/listeninghistory', methods=['GET', 'POST'])
+# def listening_history():
+#     if request.method == 'POST':
+#         numOfTracks = request.form['numOfTracks']
+#         return redirect(url_for('displayListeningHistory', numOfTracks=numOfTracks))
+#     return render_template('listeningHistory.html')
+
+@app.route('/displayListeningHistory', methods=['GET'])
+def display_listening_history():
+    numOfTracks = request.args.get('numOfTracks', type=int)
+    tracks = userPlayback(numOfTracks)
+    return render_template('displayListeningHistory.html', tracks=tracks)
